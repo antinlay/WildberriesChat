8cb41c373dcfb686f97ce5911414c6d663ac8783
Janiecee <janiecee@student.21-school.ru>
2024-06-06 04:07:36 +0300

    First commit

---
+++ WildberriesChat/WildberriesChat/WildberriesChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildberriesChatApp.swift
+//  WildberriesChat
+//
+//  Created by Janiece Eleonour on 06.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WildberriesChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
