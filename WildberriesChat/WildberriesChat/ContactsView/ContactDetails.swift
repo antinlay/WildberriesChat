5bd5f55e9d54bcc771f5ccde190595604f246833
Janiecee <janiecee@student.21-school.ru>
2024-06-17 10:44:01 +0300

    Сверстана деталька  контакта

--- WildberriesChat/WildberriesChat/ContactsView/ContactDetails.swift
+++ WildberriesChat/WildberriesChat/ContactsView/ContactDetails.swift
@@ -11,21 +11,54 @@ struct ContactDetails: View {
     var contact: Contact
     
     var body: some View {
-        Circle()
-            .fill(.neutralReverse)
-            .overlay (
-                Image(.person)
-            )
-            .frame(width: 200)
-        Text(contact.name)
-            .font(FontStyles.headingSecond)
-        Text(contact.phoneNumber.applyPhoneMask())
-            .font(FontStyles.subheadingSecond)
-            .fontWeight(.regular)
-            .foregroundStyle(.appGray)
-        
-        HStack {
+        VStack {
+            Circle()
+                .fill(.neutralReverse)
+                .overlay (
+                    Image(.person)
+                )
+                .frame(width: 200)
+                .padding()
+            Text(contact.name)
+                .font(FontStyles.headingSecond)
+            Text(contact.phoneNumber.applyPhoneMask())
+                .font(FontStyles.subheadingSecond)
+                .fontWeight(.regular)
+                .foregroundStyle(.appGray)
             
+            HStack(spacing: 12) {
+                Capsule()
+                    .stroke(.accent, lineWidth: 1.67)
+                    .overlay(Image(.Social.twitter))
+                    .onTapGesture {
+                        //
+                    }
+                
+                Capsule()
+                    .stroke(.accent, lineWidth: 1.67)
+                    .overlay(Image(.Social.instagram))
+                    .onTapGesture {
+                        //
+                    }
+                
+                Capsule()
+                    .stroke(.accent, lineWidth: 1.67)
+                    .overlay(Image(.Social.linkedin))
+                    .onTapGesture {
+                        //
+                    }
+                
+                Capsule()
+                    .stroke(.accent, lineWidth: 1.67)
+                    .overlay(Image(.Social.facebook))
+                    .onTapGesture {
+                        //
+                    }
+                
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 40)
+            .padding()
         }
     }
 }
