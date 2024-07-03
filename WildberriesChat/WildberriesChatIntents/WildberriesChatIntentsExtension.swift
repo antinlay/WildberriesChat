16f57ec934b3d08bd4e866c35c536f4c0ada4e39
Janiecee <janiecee@student.21-school.ru>
2024-07-03 11:43:54 +0300

    Добавлен intent для примера

---
+++ WildberriesChat/WildberriesChatIntents/WildberriesChatIntentsExtension.swift
@@ -0,0 +1,12 @@
+//
+//  WildberriesChatIntentsExtension.swift
+//  WildberriesChatIntents
+//
+//  Created by Janiece Eleonour on 01.07.2024.
+//
+
+import AppIntents
+
+@main
+struct WildberriesChatIntentsExtension: AppIntentsExtension {
+}
