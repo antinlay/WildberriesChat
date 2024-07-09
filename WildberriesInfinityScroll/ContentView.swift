53ddf8a0fc00ceb7c33d67a91f36aab72577e2de
Janiecee <janiecee@student.21-school.ru>
2024-07-10 01:55:16 +0300

    Init project

---
+++ WildberriesInfinityScroll/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WildberriesInfinityScroll
+//
+//  Created by Janiece Eleonour on 09.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        ScrollView {
+            Text("🍪")
+                .font(.system(size: 300))
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
